00b2911342708f4db9ca2954a6ca7d5ae9076553
Shivam <shivamjhax@gmail.com>
2024-05-28 01:07:44 +0530

    Fetching and displaying movies from tmdb

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import ShowMovies from '../components/ShowMovies';
 
 export default function Home() {
+  const [searchParams] = useSearchParams();
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const key = import.meta.env.VITE_TMDB_API;
+  const genre=searchParams.get("genre") || "fetchTrending";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3${
+            genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+          }?api_key=${key}&language=en-US&page=1`
+        );
+        const data = await res.json();
+        setMovies(data.results);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [genre]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className='min-h-screen'>Home</div>
-  )
+    <div className='min-h-screen'>
+      <ShowMovies movies={movies}/>
+    </div>
+  );
 }
---
+++ Frontend/src/components/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { FiThumbsUp } from 'react-icons/fi';
+
+export default function Card({ movie }) {
+    return (
+      <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
+        <Link to={`/movie/${movie.id}`}>
+          <img
+            src={`https://image.tmdb.org/t/p/original/${
+              movie.backdrop_path || movie.poster_path
+            }`}
+            width={500}
+            height={300}
+            className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
+          ></img>
+          <div className='p-2'>
+            <p className='line-clamp-2 text-md'>{movie.overview}</p>
+            <h2 className='text-lg font-bold truncate'>
+              {movie.title || movie.name}
+            </h2>
+            <p className='flex items-center'>
+              {movie.release_date || movie.first_air_date}
+              <FiThumbsUp className='h-5 mr-1 ml-3' />
+              {movie.vote_count}
+            </p>
+          </div>
+        </Link>
+      </div>
+    );
+  }
