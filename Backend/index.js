abdffdbe3e3eaaabc022c6eb013f7dd9e6cec516
Shivam <shivamjhax@gmail.com>
2024-06-05 02:01:41 +0530

    Added pre installation for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.route.js"
 import userRoute from "./routes/user.route.js"
 import cookieParser from 'cookie-parser';
 import reviewRoute from "./routes/review.route.js"
+import path from "path"
 
 
 dotenv.config();
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err);
 })
+
+const __dirname = path.resolve();
 
 const app=express();
 app.use(express.json());
@@ -23,6 +26,11 @@ app.use("/api/auth",authRoute)
 app.use("/api/user",userRoute)
 app.use("/api/reviews",reviewRoute)
 
+app.use(express.static(path.join(__dirname, '/Frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
+})
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
